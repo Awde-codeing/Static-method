75dade0938f433b19f62bd24114c0652e1224404
Awde <Thomas.m.Riemer@gmail.com>
2025-08-25 08:36:38 +0200

    update to metode og kald

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Goodbye, World!");
+            // Opret personer
+            Person p1 = new Person("Anna", "Hansen", "Vej 1", "1000", "København");
+            Person p2 = new Person("Lars", "Olsen", "Vej 2", "8000", "Aarhus");
+
+            // Kald instansmetoden
+            p1.SkrivPersonInstans();
+            p2.SkrivPersonInstans();
+
+            // Kald statisk metode
+            Person.SkrivInstans(p1);
+            Person.SkrivInstans(p2);
         }
     }
 }
